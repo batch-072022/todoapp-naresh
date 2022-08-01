98275ac748dea9c200e73a1834efc34f402acd67
Naresh Kumar H <nareshkumarh@live.com>
2022-08-01 18:06:36 +0530

    Added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+//http://localhost:4200/register
+//http://localhost:4200/login
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'register', component: RegisterComponent },
+  {path:'login', component: LoginComponent},
+  {path:'listuser', component:ListUsersComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
